1e4bfd013d210724ad5ef63442b04e4697cbf84e
Yang Wang <yangwangx@gmail.com>
2019-05-15 10:48:13 -0400

    updates

--- ccdnet_1/faces/CMD_gan/CMD_eval.sh
+++ ccdnet_1/faces/CMD_gan/CMD_eval.sh
@@ -1,8 +1,11 @@
 CUDA_VISIBLE_DEVICES=0 python ../main.py --evalMode --tDown 1 \
 --dither_mode nodither \
---initModel pretrained/ccdnet1_gan_faces_nodither_ep30.pt
+--initModel pretrained/ccdnet1_gan_faces_nodither_ep20.pt
 
+##  {'PSNR': 32.11, 'PSNR_gif': 30.99, 'SSIM': 0.902, 'SSIM_gif': 0.868}
 
 CUDA_VISIBLE_DEVICES=0 python ../main.py --evalMode --tDown 1 \
 --dither_mode dither \
 --initModel pretrained/ccdnet1_gan_faces_dither_ep30.pt
+
+## {'PSNR': 33.68, 'PSNR_gif': 28.21, 'SSIM': 0.940, 'SSIM_gif': 0.746}
--- ccdnet_1/faces/CMD_nogan/CMD_eval.sh
+++ ccdnet_1/faces/CMD_nogan/CMD_eval.sh
@@ -2,7 +2,10 @@ CUDA_VISIBLE_DEVICES=0 python ../main.py --evalMode --tDown 1 \
 --dither_mode nodither \
 --initModel pretrained/ccdnet1_nogan_faces_nodither_ep60.pt
 
+## {'PSNR': 32.83, 'PSNR_gif': 30.99, 'SSIM': 0.918, 'SSIM_gif': 0.868}
 
 CUDA_VISIBLE_DEVICES=0 python ../main.py --evalMode --tDown 1 \
 --dither_mode dither \
 --initModel pretrained/ccdnet1_nogan_faces_dither_ep60.pt
+
+## {'PSNR': 33.93, 'PSNR_gif': 28.21, 'SSIM': 0.944, 'SSIM_gif': 0.746}
